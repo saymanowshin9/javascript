0eabf81dea8667928ff3548d2988e8f60f2154b7
sayma nowshin <124274759+saymanowshin9@users.noreply.github.com>
2023-05-31 19:56:10 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+//text change
+function textChange(){
+document.getElementById('h2').innerHTML ="javascript is front end's light weight functional object oriented scripting language-which helps to build a interactive website."	
+}
